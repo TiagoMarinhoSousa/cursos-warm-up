22bdbb15d8ea50620a3f5042644744ca9d7fe67a
Tiago Marinho <tiago.sousa@picpay.com>
2025-08-26 17:08:07 -0300

    aulas 1, 2 e 3

---
+++ formacao-typescript/dist/js/types/Armazenador.js
@@ -0,0 +1,16 @@
+export class Armazenador {
+    constructor() { }
+    static salvar(chave, valor) {
+        localStorage.setItem(chave, JSON.stringify(valor));
+    }
+    static obter(chave, reviver) {
+        const valor = localStorage.getItem(chave);
+        if (valor === null) {
+            return null;
+        }
+        if (reviver) {
+            return JSON.parse(valor, reviver);
+        }
+        return JSON.parse(valor);
+    }
+}
