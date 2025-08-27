5b3cf1b5d0f0289df4b87e9dce98c0ac6da39e5b
Tiago Marinho <tiago.sousa@picpay.com>
2025-08-27 15:32:57 -0300

    aula 2

--- typescript-curso-3/app/src/decorators/logar-tempo-de-execucao.ts
+++ typescript-curso-3/app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,11 +1,17 @@
-export function logarTempoDeExecucao() {
+export function logarTempoDeExecucao(emSegundos: boolean = false) {
     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
         const metodoOriginal = descriptor.value;
         descriptor.value = function (...args: any[]) {
+            let unidade = 'milissegundos';
+            let divisor = 1;
+            if (emSegundos) {
+                divisor = 1000;
+                unidade = 'segundos';
+            }
             const t1 = performance.now();
             const resultado = metodoOriginal.apply(this, args);
             const t2 = performance.now();
-            console.log(`Tempo de execução do método ${propertyKey}: ${(t2 - t1) / 1000} segundos`);
+            console.log(`Tempo de execução do método ${propertyKey}: ${(t2 - t1) / divisor} ${unidade}`);
             return resultado;
         };
         return descriptor
